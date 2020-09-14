fee373903d06dabb2d665bb98617896a44ca4ee6
Paula Gomes <gomes.pcristina@gmail.com>
2020-09-13 22:43:03 -0300

    adding new database to route

--- src/paths/path_tasks.js
+++ src/paths/path_tasks.js
@@ -1,19 +1,6 @@
-const tasksToDo = require('../views/template_todoapp');
-
-const db = [
-
-    { name: 'Paula', tasks: [{ title: 'Academia', 'details': 'Malhar as 9am' }] },
-    { name: 'Julia', tasks: [{ title: 'yoga', 'details': ' Aulas terças e quintas' }] }
-];
+const  taskShow = require('../controller/task_controller');
 
 module.exports = (app) => {
 
-    app.get('/:name', (req, res) => {
-
-      
-        for (let i = 0; i < db.length; i++) {
-
-            if (req.params.name == db[i].name)
-               res.send(tasksToDo(db[i].tasks));
-        }
-    })};
+    app.get('/', taskShow.showTask());
+}
